c389898308715a3a5b0ee29136997fabf5c9aa4e
Geraldo <geraldomarioaligoey@gmail.com>
2022-01-10 22:59:35 +0800

    feat: Extends TransactionCategoryRepository with JpaRepository

--- src/main/java/com/gma/challenge/beruang/repo/TransactionCategoryRepository.java
+++ src/main/java/com/gma/challenge/beruang/repo/TransactionCategoryRepository.java
@@ -1,10 +1,13 @@
 package com.gma.challenge.beruang.repo;
 
+import com.gma.challenge.beruang.model.TransactionCategory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 @Transactional
-public interface TransactionCategoryRepository {
+public interface TransactionCategoryRepository extends JpaRepository<TransactionCategory, Long> {
   
 }
