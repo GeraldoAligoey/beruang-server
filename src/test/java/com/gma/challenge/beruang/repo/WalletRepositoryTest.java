11c6a8a58609361c8e76dd46e68d6245d6447998
Geraldo <geraldomarioaligoey@gmail.com>
2022-01-10 22:40:03 +0800

    test: Add wallet repository delete test and ordering of test execution

--- src/test/java/com/gma/challenge/beruang/repo/WalletRepositoryTest.java
+++ src/test/java/com/gma/challenge/beruang/repo/WalletRepositoryTest.java
@@ -1,21 +1,27 @@
 package com.gma.challenge.beruang.repo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import com.gma.challenge.beruang.model.Wallet;
 import com.gma.challenge.beruang.util.WalletDataSample;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import lombok.extern.slf4j.Slf4j;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DataJpaTest
 @Slf4j
 public class WalletRepositoryTest {
@@ -29,6 +35,7 @@ public class WalletRepositoryTest {
   }
 
   @Test
+  @Order(1)
   public void testFindById() {
     log.info("WalletRepositoryTest - testFindById");
 
@@ -42,10 +49,28 @@ public class WalletRepositoryTest {
   }
   
   @Test
+  @Order(2)
   public void testFindAll() {
     log.info("WalletRepositoryTest - testFindAll");
 
     List<Wallet> wallets = walletRepository.findAll();
     assertEquals(2, wallets.size());
+  }
+
+  @Test
+  @Order(3)
+  public void testDelete() {
+    log.info("WalletRepositoryTest - testDelete");
+
+    walletRepository.deleteById(1l);
+    Wallet wallet = null;
+
+    try {
+      wallet = walletRepository.findById(1l).get();
+    } catch(NoSuchElementException e) {
+      log.info(e.getMessage());
+    }
+
+    assertNull(wallet);
   }
 }
