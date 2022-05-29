4069bf1251d5d8df2a8f46a2e756fab929248956
Geraldo <geraldomarioaligoey@gmail.com>
2022-05-29 22:56:50 +0800

    config: Add sample data to be initialized

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into category(id, name, expense, icon, color, user_defined, active) values(1, 'salary', 0, 'icon', 'blue', 0, 1);
+insert into category(id, name, expense, icon, color, user_defined, active) values(2, 'transport', 1, 'icon', 'blue', 0, 1);
+insert into category(id, name, expense, icon, color, user_defined, active) values(3, 'food and drinks', 1, 'icon', 'blue', 0, 1);
+insert into category(id, name, expense, icon, color, user_defined, active) values(4, 'rental fee', 1, 'icon', 'blue', 0, 1);
+insert into category(id, name, expense, icon, color, user_defined, active) values(5, 'part time', 0, 'icon', 'blue', 0, 1);
+insert into category(id, name, expense, icon, color, user_defined, active) values(6, 'games', 1, 'icon', 'blue', 0, 1);
+insert into category(id, name, expense, icon, color, user_defined, active) values(7, 'online services', 1, 'icon', 'blue', 0, 1);
