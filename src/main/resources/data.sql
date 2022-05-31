6eef1130d2d214458f0e77d1aba8c262e1b2cdd8
Geraldo <geraldomarioaligoey@gmail.com>
2022-05-31 23:35:48 +0800

    fix: Remove id column from data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-insert into category(id, name, expense, icon, color, user_defined, active) values(1, 'salary', 0, 'icon', 'blue', 0, 1);
-insert into category(id, name, expense, icon, color, user_defined, active) values(2, 'transport', 1, 'icon', 'blue', 0, 1);
-insert into category(id, name, expense, icon, color, user_defined, active) values(3, 'food and drinks', 1, 'icon', 'blue', 0, 1);
-insert into category(id, name, expense, icon, color, user_defined, active) values(4, 'rental fee', 1, 'icon', 'blue', 0, 1);
-insert into category(id, name, expense, icon, color, user_defined, active) values(5, 'part time', 0, 'icon', 'blue', 0, 1);
-insert into category(id, name, expense, icon, color, user_defined, active) values(6, 'games', 1, 'icon', 'blue', 0, 1);
-insert into category(id, name, expense, icon, color, user_defined, active) values(7, 'online services', 1, 'icon', 'blue', 0, 1);
+insert into category(name, expense, icon, color, user_defined, active) values('salary', 0, 'icon', 'blue', 0, 1);
+insert into category(name, expense, icon, color, user_defined, active) values('transport', 1, 'icon', 'blue', 0, 1);
+insert into category(name, expense, icon, color, user_defined, active) values('food and drinks', 1, 'icon', 'blue', 0, 1);
+insert into category(name, expense, icon, color, user_defined, active) values('rental fee', 1, 'icon', 'blue', 0, 1);
+insert into category(name, expense, icon, color, user_defined, active) values('part time', 0, 'icon', 'blue', 0, 1);
+insert into category(name, expense, icon, color, user_defined, active) values('games', 1, 'icon', 'blue', 0, 1);
+insert into category(name, expense, icon, color, user_defined, active) values('online services', 1, 'icon', 'blue', 0, 1);
