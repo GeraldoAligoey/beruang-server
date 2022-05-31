1bd59e7e62484dca1a0c6df2f820e461a3f33624
Geraldo <geraldomarioaligoey@gmail.com>
2022-05-31 23:16:08 +0800

    feat: Add additional mapper

--- src/main/java/com/gma/challenge/beruang/util/Mapper.java
+++ src/main/java/com/gma/challenge/beruang/util/Mapper.java
@@ -1,6 +1,8 @@
 package com.gma.challenge.beruang.util;
 
 import com.gma.challenge.beruang.data.CategoryData;
+import com.gma.challenge.beruang.data.NewCategoryRequestData;
+import com.gma.challenge.beruang.data.UpdateCategoryData;
 import com.gma.challenge.beruang.domain.Category;
 
 public class Mapper {
@@ -15,6 +17,27 @@ public class Mapper {
     categoryData.setActive(category.isActive());
 
     return categoryData;
+  }
+
+  public static Category toCategory(NewCategoryRequestData newCategoryData) {
+    Category category = new Category();
+    category.setName(newCategoryData.getName());
+    category.setExpense(newCategoryData.getExpense());
+    category.setIcon(newCategoryData.getIcon());
+    category.setColor(newCategoryData.getColor());
+    category.setActive(true);
+
+    return category;
+
+  }
+
+  public static Category updateCategory(Category category, UpdateCategoryData categoryData) {
+    category.setName(categoryData.getName());
+    category.setExpense(categoryData.getExpense());
+    category.setIcon(categoryData.getIcon());
+    category.setColor(categoryData.getColor());
+
+    return category;
   }
 
 }
