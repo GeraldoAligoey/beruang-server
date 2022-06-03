25afd988efae5407e9eca0360955ae532b54ec77
Geraldo <geraldomarioaligoey@gmail.com>
2022-06-03 23:56:55 +0800

    fix: Update CategoryWriteServiceImpl to behave according to the OAS

--- src/main/java/com/gma/challenge/beruang/service/CategoryWriteServiceImpl.java
+++ src/main/java/com/gma/challenge/beruang/service/CategoryWriteServiceImpl.java
@@ -1,5 +1,6 @@
 package com.gma.challenge.beruang.service;
 
+import javax.persistence.EntityNotFoundException;
 
 import com.gma.challenge.beruang.data.CategoryData;
 import com.gma.challenge.beruang.data.CategoryResponseData;
@@ -7,6 +8,7 @@ import com.gma.challenge.beruang.data.NewCategoryRequestData;
 import com.gma.challenge.beruang.data.UpdateCategoryData;
 import com.gma.challenge.beruang.data.UpdateCategoryRequestData;
 import com.gma.challenge.beruang.domain.Category;
+import com.gma.challenge.beruang.exception.CategoryNotFoundException;
 import com.gma.challenge.beruang.repo.CategoryRepository;
 import com.gma.challenge.beruang.util.Mapper;
 import com.gma.challenge.beruang.util.Validator;
@@ -39,20 +41,32 @@ public class CategoryWriteServiceImpl implements CategoryWriteService {
 
   @Override
   public CategoryResponseData updateCategory(Long id, UpdateCategoryRequestData updateCategoryRequestData) {
+    Validator.validateUpdateCategoryRequestData(updateCategoryRequestData);
     UpdateCategoryData updateCategoryData = updateCategoryRequestData.getCategory();
 
-    Category category = categoryRepository.getReferenceById(id);
-    CategoryData categoryData = Mapper.toCategoryData(categoryRepository.saveAndFlush(Mapper.updateCategory(category, updateCategoryData)));
+    try {
+      Category category = categoryRepository.getReferenceById(id);
+      CategoryData categoryData = Mapper
+          .toCategoryData(categoryRepository.saveAndFlush(Mapper.updateCategory(category, updateCategoryData)));
 
-    CategoryResponseData categoryResponseData = new CategoryResponseData();
-    categoryResponseData.category(categoryData);
+      CategoryResponseData categoryResponseData = new CategoryResponseData();
+      categoryResponseData.category(categoryData);
 
-    return categoryResponseData;
+      return categoryResponseData;
+
+    } catch (EntityNotFoundException ex) {
+      throw new CategoryNotFoundException("Invalid category id");
+    }
   }
 
   @Override
   public void deleteCategory(Long id) {
-    categoryRepository.delete(categoryRepository.getReferenceById(id));
+    Category category = categoryRepository.findById(id).orElse(null);
+    if (category != null) {
+      categoryRepository.delete(category);
+    } else {
+      throw new CategoryNotFoundException("Invalid category id");
+    }
   }
-  
+
 }
