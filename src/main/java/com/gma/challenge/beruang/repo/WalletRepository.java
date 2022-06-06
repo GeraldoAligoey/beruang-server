986e1fb9cc5d9eafb264bd88126ac99214c0c9dc
Geraldo <geraldomarioaligoey@gmail.com>
2022-06-06 16:41:10 +0800

    feat: Add findByCategoryIds in WalletRepository

--- src/main/java/com/gma/challenge/beruang/repo/WalletRepository.java
+++ src/main/java/com/gma/challenge/beruang/repo/WalletRepository.java
@@ -1,14 +1,20 @@
 package com.gma.challenge.beruang.repo;
 
 
-import com.gma.challenge.beruang.domain.Wallet;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.gma.challenge.beruang.domain.Wallet;
 
 @Repository
 @Transactional
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
+
+  @Query(value = "select * from wallet where wallet.id in (select w_c.wallet_id from wallet_categories w_c where w_c.categories_id in :categoryIds)", nativeQuery = true)
+  public List<Wallet> findByCategoryIds(List<Long> categoryIds);
   
 }
