82678f3bca1ebebc1a8156f38c73b45ecfd9faee
Geraldo <geraldomarioaligoey@gmail.com>
2022-06-06 23:16:58 +0800

    feat: Add WalletWriteService

---
+++ src/main/java/com/gma/challenge/beruang/service/WalletWriteService.java
@@ -0,0 +1,14 @@
+package com.gma.challenge.beruang.service;
+
+import com.gma.challenge.beruang.data.NewWalletRequestData;
+import com.gma.challenge.beruang.data.UpdateWalletRequestData;
+import com.gma.challenge.beruang.data.WalletResponseData;
+
+public interface WalletWriteService {
+  
+  public WalletResponseData createWallet(NewWalletRequestData newWalletRequestData);
+
+  public WalletResponseData updateWallet(Long walletId, UpdateWalletRequestData updateWalletRequestData);
+
+  public void deleteWallet(Long walletId);
+}
