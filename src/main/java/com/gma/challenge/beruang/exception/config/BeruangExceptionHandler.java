e104e4a05a5e3570712b204c5da3972e28e9418a
Geraldo <geraldomarioaligoey@gmail.com>
2022-06-06 16:41:45 +0800

    feat: Add WalletNotFoundException and its handling

--- src/main/java/com/gma/challenge/beruang/exception/config/BeruangExceptionHandler.java
+++ src/main/java/com/gma/challenge/beruang/exception/config/BeruangExceptionHandler.java
@@ -2,11 +2,6 @@ package com.gma.challenge.beruang.exception.config;
 
 import java.util.List;
 
-import com.gma.challenge.beruang.data.GenericErrorModelData;
-import com.gma.challenge.beruang.data.GenericErrorModelErrorsData;
-import com.gma.challenge.beruang.exception.CategoryNotFoundException;
-import com.gma.challenge.beruang.exception.IncompleteRequestDataException;
-
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +9,12 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.gma.challenge.beruang.data.GenericErrorModelData;
+import com.gma.challenge.beruang.data.GenericErrorModelErrorsData;
+import com.gma.challenge.beruang.exception.CategoryNotFoundException;
+import com.gma.challenge.beruang.exception.IncompleteRequestDataException;
+import com.gma.challenge.beruang.exception.WalletNotFoundException;
 
 @ControllerAdvice
 public class BeruangExceptionHandler extends ResponseEntityExceptionHandler {
@@ -35,6 +36,11 @@ public class BeruangExceptionHandler extends ResponseEntityExceptionHandler {
 
   @ExceptionHandler(CategoryNotFoundException.class)
   public ResponseEntity<Object> handleCategoryNotFoundException(CategoryNotFoundException ex, WebRequest request) {
+    return handleExceptionInternal(ex, null, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+  }
+
+  @ExceptionHandler(WalletNotFoundException.class)
+  public ResponseEntity<Object> handleWalletNotFoundException(WalletNotFoundException ex, WebRequest request) {
     return handleExceptionInternal(ex, null, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
   }
 
