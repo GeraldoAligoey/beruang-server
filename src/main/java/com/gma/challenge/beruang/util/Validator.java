80ff20c544658c0723f8142b656e11ca579cc982
Geraldo <geraldomarioaligoey@gmail.com>
2022-06-07 22:56:54 +0800

    feat: Add wallet validators

--- src/main/java/com/gma/challenge/beruang/util/Validator.java
+++ src/main/java/com/gma/challenge/beruang/util/Validator.java
@@ -5,8 +5,12 @@ import java.util.Arrays;
 import java.util.List;
 
 import com.gma.challenge.beruang.data.NewCategoryRequestData;
+import com.gma.challenge.beruang.data.NewWalletData;
+import com.gma.challenge.beruang.data.NewWalletRequestData;
 import com.gma.challenge.beruang.data.UpdateCategoryData;
 import com.gma.challenge.beruang.data.UpdateCategoryRequestData;
+import com.gma.challenge.beruang.data.UpdateWalletData;
+import com.gma.challenge.beruang.data.UpdateWalletRequestData;
 import com.gma.challenge.beruang.exception.IncompleteRequestDataException;
 
 public class Validator {
@@ -62,4 +66,54 @@ public class Validator {
       throw new IncompleteRequestDataException(message.concat(Arrays.asList(dataFields).toString()));
     }
   }
+
+  public static void validateNewWalletRequestData(NewWalletRequestData newWalletRequestData) {
+    String message = "Missing data field(s): ";
+    List<String> dataFields = new ArrayList<>();
+
+    NewWalletData requestData = newWalletRequestData.getWallet();
+
+    if (requestData.getName() == null || requestData.getName().isEmpty() || requestData.getName().isBlank()) {
+      dataFields.add("name");
+    }
+
+    if (requestData.getDefaultCurrencyCode() == null || requestData.getDefaultCurrencyCode().isEmpty() || requestData.getDefaultCurrencyCode().isBlank()) {
+      dataFields.add("defaultCurrencyCode");
+    }
+
+    if (requestData.getCategories() == null || requestData.getCategories().isEmpty()) {
+      dataFields.add("categories");
+    }
+
+    if (!dataFields.isEmpty()) {
+      throw new IncompleteRequestDataException(message.concat(Arrays.asList(dataFields).toString()));
+    }
+  }
+
+  public static void validateUpdateWalletRequestData(UpdateWalletRequestData updateWalletRequestData) {
+    UpdateWalletData updateWalletData = updateWalletRequestData.getWallet();
+
+    if (updateWalletData.getName() != null && !updateWalletData.getName().isBlank()) {
+      return;      
+    }
+
+    if (updateWalletData.getDefaultCurrencyCode() != null && !updateWalletData.getDefaultCurrencyCode().isBlank()) {
+      return;      
+    }
+
+    if (updateWalletData.getDefaultWallet() != null) {
+      return;      
+    }
+
+    if (updateWalletData.getInitialBalanceAmount() == null) {
+      return;      
+    }
+
+    if (updateWalletData.getCategories() == null && !updateWalletData.getCategories().isEmpty()) {
+      return;      
+    }
+
+    throw new IncompleteRequestDataException("Missing data field(s) to be updated");
+  }
+
 }
