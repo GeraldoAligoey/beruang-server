f7098870dcf793a6352e06691f2e2943b5f1673a
Geraldo <geraldomarioaligoey@gmail.com>
2022-06-07 22:58:01 +0800

    feat: Add mapper functions

--- src/main/java/com/gma/challenge/beruang/util/Mapper.java
+++ src/main/java/com/gma/challenge/beruang/util/Mapper.java
@@ -1,11 +1,16 @@
 package com.gma.challenge.beruang.util;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import com.gma.challenge.beruang.data.CategoryData;
 import com.gma.challenge.beruang.data.NewCategoryRequestData;
+import com.gma.challenge.beruang.data.NewWalletData;
+import com.gma.challenge.beruang.data.NewWalletRequestData;
 import com.gma.challenge.beruang.data.UpdateCategoryData;
+import com.gma.challenge.beruang.data.UpdateWalletData;
 import com.gma.challenge.beruang.data.WalletData;
 import com.gma.challenge.beruang.domain.Category;
 import com.gma.challenge.beruang.domain.Wallet;
@@ -24,6 +29,18 @@ public class Mapper {
     return categoryData;
   }
 
+  private static Category toCategory(CategoryData categoryData) {
+    Category category = new Category();
+    category.setId(categoryData.getId());
+    category.setName(categoryData.getName());
+    category.setExpense(categoryData.getExpense());
+    category.setIcon(categoryData.getIcon());
+    category.setColor(categoryData.getColor());
+    category.setActive(true);
+
+    return category;
+  }
+
   public static Category toCategory(NewCategoryRequestData newCategoryData) {
     Category category = new Category();
     category.setName(newCategoryData.getName());
@@ -33,7 +50,6 @@ public class Mapper {
     category.setActive(true);
 
     return category;
-
   }
 
   public static Category updateCategory(Category category, UpdateCategoryData categoryData) {
@@ -62,4 +78,59 @@ public class Mapper {
 
     return walletData;
   }
+
+  public static Wallet toWallet(NewWalletRequestData newWalletRequestData) {
+    NewWalletData newWalletData = newWalletRequestData.getWallet();
+    Wallet wallet = new Wallet();
+    wallet.setName(newWalletData.getName());
+    wallet.setDefaultCurrencyCode(newWalletData.getDefaultCurrencyCode());
+
+    Set<Category> categories = newWalletData.getCategories()
+      .stream()
+      .map(categoryData -> toCategory(categoryData))
+      .collect(Collectors.toSet());
+    
+    wallet.setCategories(categories);
+
+    if (newWalletData.getInitialBalanceAmount() == null) {
+      wallet.setInitialBalanceAmount(BigDecimal.ZERO);
+    } else {
+      wallet.setInitialBalanceAmount(newWalletData.getInitialBalanceAmount());
+    }
+
+    if (newWalletData.getDefaultWallet() != null) {
+      wallet.setDefaultWallet(newWalletData.getDefaultWallet());
+    }
+
+    return wallet;
+  }
+
+  public static Wallet updateWallet(Wallet wallet, UpdateWalletData updateWalletData) {
+    if (updateWalletData.getName() != null && !updateWalletData.getName().isBlank()) {
+      wallet.setName(updateWalletData.getName());
+    }
+
+    if (updateWalletData.getDefaultWallet() != null) {
+      wallet.setDefaultWallet(updateWalletData.getDefaultWallet());
+    }
+
+    if (updateWalletData.getDefaultCurrencyCode() != null && !updateWalletData.getDefaultCurrencyCode().isBlank()) {
+      wallet.setDefaultCurrencyCode(updateWalletData.getDefaultCurrencyCode());
+    }
+
+    if (updateWalletData.getInitialBalanceAmount() != null) {
+      wallet.setInitialBalanceAmount(updateWalletData.getInitialBalanceAmount());
+    }
+
+    if (updateWalletData.getCategories() != null && !updateWalletData.getCategories().isEmpty()) {
+      Set<Category> categories = updateWalletData.getCategories()
+        .stream()
+        .map(categoryData -> toCategory(categoryData))
+        .collect(Collectors.toSet());
+      wallet.setCategories(categories);
+    }
+
+    return wallet;
+  }
+
 }
