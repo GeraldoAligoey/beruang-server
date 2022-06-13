9a5fcf5c7d3efb7f05d291c249dd05dd11251b83
Geraldo <geraldomarioaligoey@gmail.com>
2022-06-13 23:45:21 +0800

    fix: Add delete budget and budget_categories in test script

--- src/test/resources/sql/testFindCategories_single.sql
+++ src/test/resources/sql/testFindCategories_single.sql
@@ -1,4 +1,6 @@
 delete from wallet_categories;
+delete from budget_categories;
+delete from budget;
 delete from wallet;
 delete from category;
 insert into category(name, expense, icon, color, user_defined, active) values('salary', 0, 'icon', 'blue', 0, 1);
