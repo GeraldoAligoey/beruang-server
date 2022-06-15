5a4ec450ce62266c3a9b71ca186b7c4db27a6dd5
Geraldo <geraldomarioaligoey@gmail.com>
2022-06-15 23:22:43 +0800

    feat: Add budget related validators

--- src/main/java/com/gma/challenge/beruang/util/Validator.java
+++ src/main/java/com/gma/challenge/beruang/util/Validator.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import com.gma.challenge.beruang.data.NewBudgetRequestData;
 import com.gma.challenge.beruang.data.NewCategoryRequestData;
 import com.gma.challenge.beruang.data.NewWalletRequestData;
+import com.gma.challenge.beruang.data.UpdateBudgetRequestData;
 import com.gma.challenge.beruang.data.UpdateCategoryRequestData;
 import com.gma.challenge.beruang.data.UpdateWalletRequestData;
 import com.gma.challenge.beruang.exception.IncompleteRequestDataException;
@@ -104,6 +106,57 @@ public class Validator {
       }
 
       if (requestData.getInitialBalanceAmount() != null) {
+        return;
+      }
+
+      if (requestData.getCategoryIds() != null && !requestData.getCategoryIds().isEmpty()) {
+        return;
+      }
+    }
+
+    throw new IncompleteRequestDataException("Missing data field(s) to be updated");
+  }
+
+  public static void validateNewBudgetRequestData(NewBudgetRequestData requestData) {
+    if (requestData == null) {
+      throw new IncompleteRequestDataException("Invalid request data");
+    }
+
+    String message = "Missing data field(s): ";
+    List<String> dataFields = new ArrayList<>();
+
+    if (requestData.getName() == null || requestData.getName().isEmpty() || requestData.getName().isBlank()) {
+      dataFields.add("name");
+    }
+
+    if (requestData.getPeriod() == null || requestData.getPeriod().isEmpty() || requestData.getPeriod().isBlank()) {
+      dataFields.add("period");
+    }
+
+    if (requestData.getLimitAmount() == null) {
+      dataFields.add("limitAmount");
+    }
+
+    if (requestData.getCategoryIds() == null || requestData.getCategoryIds().isEmpty()) {
+      dataFields.add("categories");
+    }
+
+    if (!dataFields.isEmpty()) {
+      throw new IncompleteRequestDataException(message.concat(Arrays.asList(dataFields).toString()));
+    }
+  }
+
+  public static void validateUpdateBudgetRequestData(UpdateBudgetRequestData requestData) {
+    if (requestData != null) {
+      if (requestData.getName() != null && !requestData.getName().isBlank()) {
+        return;
+      }
+
+      if (requestData.getPeriod() != null && !requestData.getPeriod().isBlank()) {
+        return;
+      }
+
+      if (requestData.getLimitAmount() != null) {
         return;
       }
 
