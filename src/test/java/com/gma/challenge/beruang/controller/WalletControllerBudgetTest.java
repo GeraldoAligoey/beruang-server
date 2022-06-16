b06401cb54142de58da66c1b13a1f03b824e5e61
Geraldo <geraldomarioaligoey@gmail.com>
2022-06-16 22:32:04 +0800

    test: Add WalletControllerBudgetTest #16

---
+++ src/test/java/com/gma/challenge/beruang/controller/WalletControllerBudgetTest.java
@@ -0,0 +1,131 @@
+package com.gma.challenge.beruang.controller;
+
+public class WalletControllerBudgetTest implements ControllerTest{
+
+  @Override
+  public void testCreate_validNewRequestData() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void testCreate_invalidEmptyNewRequestData() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void testCreate_invalidIncompleteNewRequestData() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void testCreate_invalidNullNewRequestData() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void testUpdate_validId_validFullRequestData() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void testUpdate_validId_validPartialRequestData() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void testUpdate_validId_invalidEmptyRequestData() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void testUpdate_validId_invalidNullRequestData() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void testUpdate_invalidId_validFullRequestData() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void testUpdate_invalidId_validPartialRequestData() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void testUpdate_invalidId_invalidEmptyRequestData() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void testUpdate_invalidId_invalidNullRequestData() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void testDelete_validId_linked() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void testDelete_validId_unlinked() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void testDelete_invalidId() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void testFindRecord_validId_recordExist() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void testFindRecord_validId_recordNotExist() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void testFindRecord_invalidId() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void testFindRecords_empty() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void testFindRecords_single() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void testFindRecords_multiple() {
+    // TODO Auto-generated method stub
+    
+  }
+  
+}
