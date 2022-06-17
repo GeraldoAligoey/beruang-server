27a17492af1c780a6417f5d9ca5703b1455e1ba9
Geraldo <geraldomarioaligoey@gmail.com>
2022-06-17 23:46:27 +0800

    test: Implement WalletControllerBudgetTest test scenarios #16

--- src/test/java/com/gma/challenge/beruang/controller/WalletControllerBudgetTest.java
+++ src/test/java/com/gma/challenge/beruang/controller/WalletControllerBudgetTest.java
@@ -1,131 +1,285 @@
 package com.gma.challenge.beruang.controller;
 
-public class WalletControllerBudgetTest implements ControllerTest{
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+import com.gma.challenge.beruang.data.BudgetData;
+import com.gma.challenge.beruang.data.BudgetResponseData;
+import com.gma.challenge.beruang.data.BudgetsResponseData;
+import com.gma.challenge.beruang.exception.BudgetNotFoundException;
+import com.gma.challenge.beruang.exception.IncompleteRequestDataException;
+import com.gma.challenge.beruang.exception.WalletNotFoundException;
+import com.gma.challenge.beruang.helper.BudgetHelper;
+import com.gma.challenge.beruang.repo.BudgetRepository;
+import com.gma.challenge.beruang.repo.WalletRepository;
+
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@ActiveProfiles("test")
+public class WalletControllerBudgetTest implements ControllerTest {
+
+  private static final Long INVALID_BUDGET_ID = Long.valueOf(-1);
+  private static final Long VALID_BUDGET_ID = Long.valueOf(1);
+  private static final Long INVALID_WALLET_ID = Long.valueOf(-1);
+  private static final Long VALID_WALLET_ID = Long.valueOf(1l);
+
+  @Autowired
+  private WalletController SUT;
+
+  @Autowired
+  private BudgetRepository budgetRepository;
+
+  @Autowired
+  private WalletRepository walletRepository;
+
+  @Test
   @Override
   public void testCreate_validNewRequestData() {
-    // TODO Auto-generated method stub
-    
+    ResponseEntity<BudgetResponseData> responseEntity = SUT.createBudget(VALID_WALLET_ID,
+        BudgetHelper.getValidNewBudgetRequestDataSample());
+    assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    assertNotNull(responseEntity.getBody().getBudget());
+    assertTrue(BudgetHelper.isBudgetResponseDataEqualsToSample(responseEntity.getBody()));
   }
 
+  @Test
   @Override
   public void testCreate_invalidEmptyNewRequestData() {
-    // TODO Auto-generated method stub
-    
+    assertThrows(IncompleteRequestDataException.class,
+        () -> SUT.createBudget(VALID_WALLET_ID, BudgetHelper.getInvalidEmptyNewBudgetRequestDataSample()));
   }
 
+  @Test
   @Override
   public void testCreate_invalidIncompleteNewRequestData() {
-    // TODO Auto-generated method stub
-    
+    assertThrows(IncompleteRequestDataException.class,
+        () -> SUT.createBudget(VALID_WALLET_ID, BudgetHelper.getInvalidEmptyNewBudgetRequestDataSample()));
   }
 
+  @Test
   @Override
   public void testCreate_invalidNullNewRequestData() {
-    // TODO Auto-generated method stub
-    
+    assertThrows(IncompleteRequestDataException.class,
+        () -> SUT.createBudget(VALID_WALLET_ID, BudgetHelper.getInvalidNullNewBudgetRequestDataSample()));
   }
 
+  @Test
+  public void testCreate_invalidWalletId_validRequestData() {
+    assertThrows(WalletNotFoundException.class,
+        () -> SUT.createBudget(INVALID_WALLET_ID, BudgetHelper.getValidNewBudgetRequestDataSample()));
+  }
+
+  @Test
+  public void testCreate_invalidWalletId_invalidIncompleteRequestData() {
+    assertThrows(IncompleteRequestDataException.class,
+        () -> SUT.createBudget(INVALID_WALLET_ID, BudgetHelper.getInvalidIncompleteNewBudgetRequestDataSample()));
+  }
+
+  @Test
+  public void testCreate_invalidWalletId_invalidEmptyRequestData() {
+    assertThrows(IncompleteRequestDataException.class,
+        () -> SUT.createBudget(INVALID_WALLET_ID, BudgetHelper.getInvalidEmptyNewBudgetRequestDataSample()));
+  }
+
+  @Test
+  public void testCreate_invalidWalletId_invalidNullRequestData() {
+    assertThrows(IncompleteRequestDataException.class,
+        () -> SUT.createBudget(INVALID_WALLET_ID, BudgetHelper.getInvalidNullNewBudgetRequestDataSample()));
+  }
+
+  @Test
   @Override
   public void testUpdate_validId_validFullRequestData() {
-    // TODO Auto-generated method stub
-    
+    ResponseEntity<BudgetResponseData> responseEntity = SUT.updateBudget(VALID_WALLET_ID, VALID_BUDGET_ID,
+        BudgetHelper.getValidFullUpdateBudgetRequestDataSample());
+    assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    assertNotNull(responseEntity.getBody().getBudget());
+    assertTrue(BudgetHelper.isUpdateBudgetResponseDataEqualsToSample(responseEntity.getBody()));
   }
 
+  @Test
   @Override
   public void testUpdate_validId_validPartialRequestData() {
-    // TODO Auto-generated method stub
-    
+    ResponseEntity<BudgetResponseData> responseEntity = SUT.updateBudget(VALID_WALLET_ID, VALID_BUDGET_ID,
+        BudgetHelper.getValidPartialUpdateBudgetRequestDataSample());
+    assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    assertNotNull(responseEntity.getBody().getBudget());
+    assertTrue(BudgetHelper.isUpdateBudgetResponseDataEqualsToPartialSample(responseEntity.getBody()));
   }
 
+  @Test
   @Override
   public void testUpdate_validId_invalidEmptyRequestData() {
-    // TODO Auto-generated method stub
-    
+    assertThrows(IncompleteRequestDataException.class,
+        () -> SUT.updateBudget(VALID_WALLET_ID, VALID_BUDGET_ID,
+            BudgetHelper.getInvalidEmptyUpdateBudgetRequestDataSample()));
   }
 
+  @Test
   @Override
   public void testUpdate_validId_invalidNullRequestData() {
-    // TODO Auto-generated method stub
-    
+    assertThrows(IncompleteRequestDataException.class,
+        () -> SUT.updateBudget(VALID_WALLET_ID, VALID_BUDGET_ID,
+            BudgetHelper.getInvalidNullUpdateBudgetRequestDataSample()));
   }
 
+  @Test
   @Override
   public void testUpdate_invalidId_validFullRequestData() {
-    // TODO Auto-generated method stub
-    
+    assertThrows(WalletNotFoundException.class,
+        () -> SUT.updateBudget(INVALID_WALLET_ID, VALID_BUDGET_ID,
+            BudgetHelper.getValidFullUpdateBudgetRequestDataSample()));
+
+    assertThrows(BudgetNotFoundException.class,
+        () -> SUT.updateBudget(VALID_WALLET_ID, INVALID_BUDGET_ID,
+            BudgetHelper.getValidFullUpdateBudgetRequestDataSample()));
+
+    assertThrows(BudgetNotFoundException.class,
+        () -> SUT.updateBudget(INVALID_WALLET_ID, INVALID_BUDGET_ID,
+            BudgetHelper.getValidFullUpdateBudgetRequestDataSample()));
   }
 
+  @Test
   @Override
   public void testUpdate_invalidId_validPartialRequestData() {
-    // TODO Auto-generated method stub
-    
+    assertThrows(WalletNotFoundException.class,
+        () -> SUT.updateBudget(INVALID_WALLET_ID, VALID_BUDGET_ID,
+            BudgetHelper.getValidPartialUpdateBudgetRequestDataSample()));
+
+    assertThrows(BudgetNotFoundException.class,
+        () -> SUT.updateBudget(VALID_WALLET_ID, INVALID_BUDGET_ID,
+            BudgetHelper.getValidPartialUpdateBudgetRequestDataSample()));
+
+    assertThrows(BudgetNotFoundException.class,
+        () -> SUT.updateBudget(INVALID_WALLET_ID, INVALID_BUDGET_ID,
+            BudgetHelper.getValidPartialUpdateBudgetRequestDataSample()));
   }
 
+  @Test
   @Override
   public void testUpdate_invalidId_invalidEmptyRequestData() {
-    // TODO Auto-generated method stub
-    
+    assertThrows(IncompleteRequestDataException.class,
+        () -> SUT.updateBudget(INVALID_WALLET_ID, VALID_BUDGET_ID,
+            BudgetHelper.getInvalidEmptyUpdateBudgetRequestDataSample()));
+
+    assertThrows(IncompleteRequestDataException.class,
+        () -> SUT.updateBudget(VALID_WALLET_ID, INVALID_BUDGET_ID,
+            BudgetHelper.getInvalidEmptyUpdateBudgetRequestDataSample()));
+
+    assertThrows(IncompleteRequestDataException.class,
+        () -> SUT.updateBudget(INVALID_WALLET_ID, INVALID_BUDGET_ID,
+            BudgetHelper.getInvalidEmptyUpdateBudgetRequestDataSample()));
   }
 
+  @Test
   @Override
   public void testUpdate_invalidId_invalidNullRequestData() {
-    // TODO Auto-generated method stub
-    
+    assertThrows(IncompleteRequestDataException.class,
+        () -> SUT.updateBudget(INVALID_WALLET_ID, VALID_BUDGET_ID,
+            BudgetHelper.getInvalidNullUpdateBudgetRequestDataSample()));
+
+    assertThrows(IncompleteRequestDataException.class,
+        () -> SUT.updateBudget(VALID_WALLET_ID, INVALID_BUDGET_ID,
+            BudgetHelper.getInvalidNullUpdateBudgetRequestDataSample()));
+
+    assertThrows(IncompleteRequestDataException.class,
+        () -> SUT.updateBudget(INVALID_WALLET_ID, INVALID_BUDGET_ID,
+            BudgetHelper.getInvalidNullUpdateBudgetRequestDataSample()));
   }
 
+  @Test
   @Override
   public void testDelete_validId_linked() {
-    // TODO Auto-generated method stub
-    
+    SUT.deleteBudget(VALID_WALLET_ID, VALID_BUDGET_ID);
+    assertNull(budgetRepository.findById(VALID_BUDGET_ID).orElse(null));
+    assertNotNull(walletRepository.findById(VALID_WALLET_ID).orElse(null));
   }
 
   @Override
   public void testDelete_validId_unlinked() {
-    // TODO Auto-generated method stub
-    
+    // Budget cannot be unlinked
   }
 
+  @Test
   @Override
   public void testDelete_invalidId() {
-    // TODO Auto-generated method stub
-    
+    assertThrows(BudgetNotFoundException.class,
+        () -> SUT.deleteBudget(INVALID_WALLET_ID, INVALID_BUDGET_ID));
+
+    assertThrows(WalletNotFoundException.class,
+        () -> SUT.deleteBudget(INVALID_WALLET_ID, VALID_BUDGET_ID));
+
+    assertThrows(BudgetNotFoundException.class,
+        () -> SUT.deleteBudget(VALID_WALLET_ID, INVALID_BUDGET_ID));
   }
 
+  @Test
+  @Sql({ "classpath:sql/testFind_empty.sql", "classpath:sql/testFindBudgets_multiple.sql" })
   @Override
   public void testFindRecord_validId_recordExist() {
-    // TODO Auto-generated method stub
-    
+    ResponseEntity<BudgetResponseData> responseEntity = SUT.findBudget(VALID_WALLET_ID, VALID_BUDGET_ID);
+    assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    assertNotNull(responseEntity.getBody().getBudget());
   }
 
+  @Test
   @Override
   public void testFindRecord_validId_recordNotExist() {
-    // TODO Auto-generated method stub
-    
+    assertThrows(BudgetNotFoundException.class, () -> SUT.findBudget(10000l, 10000l));
+    assertThrows(BudgetNotFoundException.class, () -> SUT.findBudget(VALID_WALLET_ID, 10000l));
+    assertThrows(BudgetNotFoundException.class, () -> SUT.findBudget(10000l, VALID_BUDGET_ID));
   }
 
+  @Test
   @Override
   public void testFindRecord_invalidId() {
-    // TODO Auto-generated method stub
-    
+    assertThrows(BudgetNotFoundException.class, () -> SUT.findBudget(INVALID_WALLET_ID, VALID_BUDGET_ID));
+    assertThrows(BudgetNotFoundException.class, () -> SUT.findBudget(VALID_WALLET_ID, INVALID_BUDGET_ID));
   }
 
+  @Test
+  @Sql("classpath:sql/testFind_empty.sql")
   @Override
   public void testFindRecords_empty() {
-    // TODO Auto-generated method stub
-    
+    ResponseEntity<BudgetsResponseData> responseEntity = SUT.findBudgets(VALID_WALLET_ID);
+    BudgetsResponseData responseData = responseEntity.getBody();
+
+    assertNotNull(responseData);
+    assertTrue(responseData.getBudgets().isEmpty());
   }
 
+  @Test
+  @Sql({ "classpath:sql/testFind_empty.sql", "classpath:sql/testFindBudgets_single.sql" })
   @Override
   public void testFindRecords_single() {
-    // TODO Auto-generated method stub
-    
+    ResponseEntity<BudgetsResponseData> responseEntity = SUT.findBudgets(VALID_WALLET_ID);
+    List<BudgetData> budgets = responseEntity.getBody().getBudgets();
+
+    assertTrue(budgets.size() == 1);
   }
 
+  @Test
+  @Sql({ "classpath:sql/testFind_empty.sql", "classpath:sql/testFindBudgets_multiple.sql" })
   @Override
   public void testFindRecords_multiple() {
-    // TODO Auto-generated method stub
-    
+    ResponseEntity<BudgetsResponseData> responseEntity = SUT.findBudgets(VALID_WALLET_ID);
+    List<BudgetData> budgets = responseEntity.getBody().getBudgets();
+
+    assertTrue(budgets.size() > 1);
   }
-  
+
 }
