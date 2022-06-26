02a40122dd45a00fc8d278a30b0c37da198e3702
Geraldo <geraldomarioaligoey@gmail.com>
2022-06-26 15:34:28 +0800

    feat: Add setAllDefaultFalse in walletRepository #10

--- src/main/java/com/gma/challenge/beruang/repo/WalletRepository.java
+++ src/main/java/com/gma/challenge/beruang/repo/WalletRepository.java
@@ -1,9 +1,9 @@
 package com.gma.challenge.beruang.repo;
 
-
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,5 +16,9 @@ public interface WalletRepository extends JpaRepository<Wallet, Long> {
 
   @Query(value = "select * from wallet where wallet.id in (select w_c.wallet_id from wallet_categories w_c where w_c.categories_id in :categoryIds)", nativeQuery = true)
   public List<Wallet> findByCategoryIds(List<Long> categoryIds);
-  
+
+  @Modifying
+  @Query("update Wallet w set w.defaultWallet = false where w.defaultWallet = true")
+  public void setAllDefaultFalse();
+
 }
