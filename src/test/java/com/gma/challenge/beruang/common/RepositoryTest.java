81ee9ea043bb59969ea5e5c9ec42258d62b4283c
Geraldo <geraldomarioaligoey@gmail.com>
2022-06-30 23:35:20 +0800

    feat: Add RepositoryTest interface

---
+++ src/test/java/com/gma/challenge/beruang/common/RepositoryTest.java
@@ -0,0 +1,12 @@
+package com.gma.challenge.beruang.common;
+
+public interface RepositoryTest {
+
+  public void testFindAll();
+
+  public void testCreate();
+
+  public void testUpdate();
+
+  public void testDelete();
+}
